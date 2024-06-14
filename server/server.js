a852814bc637856ed190522e349e6ec8d21de902
shankkyy <shivv2403@gmail.com>
2024-06-14 17:02:49 +0530

    u

--- server/server.js
+++ server/server.js
@@ -8,12 +8,11 @@ require('dotenv').config()
 app.use(cors())
 const bodyParser = require("body-parser");
  app.use(bodyParser.json());
-
 app.use(express.json());
 
 port = process.env.PORT||5000;
 
-    mongoose.connect('mongodb+srv://shivv2403:GZVko565JgGCVfiu@cluster0.gfuwuol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect(process.env.URL)
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
     
