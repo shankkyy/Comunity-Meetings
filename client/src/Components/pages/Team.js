8ef075c171a058443e10eeff0dcffd77558bcfb4
shankkyy <shivv2403@gmail.com>
2024-06-22 17:52:25 +0530

    jgjgj

--- client/src/Components/pages/Team.js
+++ client/src/Components/pages/Team.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card, CardBody, CardTitle, CardText, CardFooter, Container, Button } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
-import './EventList.css';
+import '../pages/EventList.css';
 
 function EventList() {
     const [events, setEvents] = useState([]);
