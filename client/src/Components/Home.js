9676e78fd674e92627f813e892930f997f77440b
shankkyy <shivv2403@gmail.com>
2024-07-22 00:25:36 +0530

    layout shift

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -7,13 +7,14 @@ import CreateEvent from './pages/CreateEvent';
 import Navbar from './Navbar';
 import UpdateEvent from './pages/UpdateEvent';
 import Footer from './Footer';
+
 function Home() {
   const location = useLocation();
 
   return (
-    <div>
+    <div style={styles.container}>
       <Navbar />
-      <div style={{ marginTop: '200px' }}>
+      <div style={styles.content}>
         <AnimatePresence mode='wait'>
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Team />} />
@@ -23,9 +24,25 @@ function Home() {
           </Routes>
         </AnimatePresence>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  content: {
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+    marginTop: '100px', // Ensure this margin matches your Navbar height to avoid layout shifts
+  },
+};
 
 export default Home;
